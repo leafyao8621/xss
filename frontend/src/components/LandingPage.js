38699ffb5679ff8c9498fca9bfb9a5b0a74d7b4d
Yi Yao <leaf_yao8621@hotmail.com>
2021-10-22 20:52:27 -0700

    Added landing page

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,25 +1,64 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
 import axios from "axios";
-import {
-    Button,
-    Form,
-    FormGroup,
-    Input,
-    Label,
-} from "reactstrap";
+import moment from "moment-timezone"
 
 class LandingPage extends Component {
     constructor(props) {
         super(props);
+        this._isMounted = false;
+        this.state = {
+            data: []
+        };
     }
-    login(e) {
-        e.preventDefault();
+    componentDidMount() {
+        this._isMounted = true;
+        this.refreshData();
+    }
+    refreshData() {
+        axios
+            .get("/api/entries/get_entries/")
+            .then((res) => {
+                if (this._isMounted) {
+                    this.setState({data: res.data.result});
+                }
+            }).catch((err) => {
+                console.log(err);
+            });
+    }
+    renderData() {
+        return (
+            this.state.data.map((item) => {
+                return (
+                    <tr key={item.id}>
+                        <td>{moment(item.created).local().format("LLL")}</td>
+                        <td>{item.sender}</td>
+                        <td>{item.content}</td>
+                    </tr>
+                )
+            })
+        )
     }
     render() {
         return (
             <main className="container">
                 <h1>Logged in as {this.props.location.state.userName}</h1>
+                <div className="container">
+                        <div className="row">
+                            <div className="col-12"></div>
+                            <table className="table table-image">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Time</th>
+                                        <th scope="col">Sender</th>
+                                        <th scope="col">Content</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    { this.renderData() }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
             </main>
         );
     }
